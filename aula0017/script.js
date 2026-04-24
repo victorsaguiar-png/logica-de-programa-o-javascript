1ce6be3ea8fe6bf8bb7c39d3b6cc94eea68541b1
victorsaguiar-png <victorgoncalvesaguiar@gmail.com>
2026-04-23 21:19:18 -0300

    Add files via upload

---
+++ aula0017/script.js
@@ -0,0 +1,69 @@
+let frutas = ["Uva", "Morango", "Laranja"]
+
+/* método push adiciona item no final da lista */
+frutas.push("Banana")
+console.log(frutas)
+
+/* Remove o ultimo item da lista */
+frutas.pop()
+console.log(frutas)
+
+/* Remove o primeiro item da lista */
+frutas.shift()
+console.log(frutas)
+
+/* Adicione no inicio da lista */
+frutas.unshift("Abacate")
+console.log(frutas)
+
+/* Mostra a quantidade de elementos na lista */
+console.log(frutas.length)
+
+/* Verifica se um valor existe na lista */
+console.log(frutas.includes("Abacate"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- aula016/script.js
+++ aula016/script.js
@@ -1,1 +1,2 @@
 let fimes = ["Vingadores", "Velozes e Furiosos", "Homem Aranha", ""]
+ 
